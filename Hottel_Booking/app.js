9e7ef2e47051bba541d6e338bb4dcc5b2d1340a8
kanhaiya <kanhaiyajee804418@gmail.com>
2024-07-04 06:11:53 +0100

    first commit

---
+++ Hottel_Booking/app.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const ExpressError = require("./Utils/ExpressError.js");
+
+
+
+
+const listings = require("./routes/listing.js");
+const reviews = require("./models/review.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public/css")));
+
+app.get("/", (req, res) => {
+  res.send("Hi, I am root");
+});
+
+
+app.use("/listings",listings);
+app.use("/listings/:id/reviews",reviews);  //parent route
+
+
+
+
+
+
+
+
+//  Error handler  (Middleware)
+
+
+// Agar koi user koi aise hmare hi domain k kisisi random route k pass request jati h to page nhi hoga (kisi se match nhi hua to hm page not to hm standerd response send krna chate h )
+app.all("*",(req,res,next)=> {
+  next(new ExpressError(404,"page Not Found"));
+});
+
+app.use((err,req,res,next) => {
+  let {statusCode, message = "Something went wrong!"} = err;
+  // res.render("error.ejs");
+  res.status(statusCode).send(message);
+});
+
+
+app.listen(8080, () => {
+  console.log("server is listining to port 8080");
+});
