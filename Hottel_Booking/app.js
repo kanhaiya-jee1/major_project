b8ed5cbea215f5df2662efa1f418038f239a3c60
kanhaiya <kanhaiyajee804418@gmail.com>
2024-07-05 12:28:01 +0100

    password

--- Hottel_Booking/app.js
+++ Hottel_Booking/app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./Utils/ExpressError.js");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
+
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -31,9 +33,33 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public/css")));
 
+//  Expression session
+
+const sessionOptions = {
+  secret: "mysupersecretcode",
+  resave: false,
+  saveUniitialized: true,
+  cookie:{
+    expires: Date.now() + 7 * 24 * 60 * 60 *1000,
+    maxAge: 7 * 24* 60 * 60 * 1000,
+    httpOnly: true,
+  },
+};
+
 app.get("/", (req, res) => {
   res.send("Hi, I am root");
 });
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+//  Middleware
+
+app.use((req,res,next)=> {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+})
 
 app.use("/listings", listings);
 app.use("/listings/:id/reviews", reviews); //parent route
--- Hottel_Booking/routes/review.js
+++ Hottel_Booking/routes/review.js
@@ -31,6 +31,7 @@ router.post(
 
     await newReview.save();
     await listing.save();
+    req.flash("success", "New Review Created!");
     res.redirect(`/listings/${listing._id}`);
   })
 );
@@ -44,7 +45,7 @@ router.delete(
 
     await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
-
+    req.flash("success", "Review Deleted!");
     res.redirect(`/listings/${id}`);
   })
 );
