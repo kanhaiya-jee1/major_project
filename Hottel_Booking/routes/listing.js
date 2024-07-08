07c31dc9a223bf0ee38609541637aa739ecad19c
kanhaiya <kanhaiyajee804418@gmail.com>
2024-07-08 18:46:04 +0100

    multer

--- Hottel_Booking/routes/listing.js
+++ Hottel_Booking/routes/listing.js
@@ -6,14 +6,21 @@ const { isLoggedIn ,isOwner,validateListing} = require("../middleware.js");
 
 const listingController = require("../controllers/listings.js")
 
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
 router
    .route("/")
    .get(WrapAsync(listingController.index))
-  .post(
-  isLoggedIn,
-  validateListing,
-  WrapAsync(listingController.createListing)
-);
+//   .post(
+//   isLoggedIn,
+//   validateListing,
+//   WrapAsync(listingController.createListing)
+// );
+
+.post((req,res)=> {
+  res.send(req.body);
+});
 
 //  New Rout
 router.get("/new", isLoggedIn, listingController.renderNewForm);
