66431fc4dad1fb8e1d46a27cab66c20d68100031
kanhaiya <kanhaiyajee804418@gmail.com>
2024-07-08 10:58:58 +0100

    rty

--- Hottel_Booking/routes/user.js
+++ Hottel_Booking/routes/user.js
@@ -8,7 +8,6 @@ const { saveRedirectUrl } = require("../middleware.js");
 const userController = require("../controllers/users.js");
 
 //  Sign up
-
 router.route("/signup")
 .get(userController.renderSignupForm)
 .post(wrapAsync(userController.signup));
